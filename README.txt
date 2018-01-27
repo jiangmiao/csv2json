5d37ef3170ad355215875c623575a470dc97c650
Miao Jiang <jiangfriend@gmail.com>
2018-01-28 04:45:01 +0800

    first commit

---
+++ README.txt
@@ -0,0 +1,34 @@
+CSV2JSON
+
+Convert CSV to JSON
+Work with [jq](https://stedolan.github.io/jq/) a powerful json querier.
+
+Installation:
+  go get github.com/jiangmiao/csv2json
+
+Usage:
+  csv2json < STDIN
+
+Options:
+  -comma string
+    	comma (default ",")
+  -no-header
+    	no header, cells will be Array instead of Object
+  -no-trim
+    	disable trim leading space
+
+Example:
+  $ cat example.csv
+  Name,Age
+  Alice,21
+  Bob,22
+  Eve,21
+
+  $ csv2json < example.csv
+  [{"Age":"21","Name":"Alice"},{"Age":"22","Name":"Bob"},{"Age":"21","Name":"Eve"}]
+
+  $ csv2json < example.csv | jq '.[]|select(.Name=="Eve")'
+  {
+    "Age": "21",
+    "Name": "Eve"
+  }
---
+++ csv2json.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bytes"
+	"encoding/csv"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func main() {
+	flagNoTrim := flag.Bool("no-trim", false, "disable trim leading space")
+	flagComma := flag.String("comma", ",", "comma")
+	flagNoHeader := flag.Bool("no-header", false, "no header, returns Array instead of Object")
+	flag.Parse()
+
+	stat, err := os.Stdin.Stat()
+	if stat.Mode()&os.ModeCharDevice != 0 {
+		// no data in stdin
+		fmt.Println(`convert csv to json from stdin
+
+Usage:
+  csv2json < STDIN
+
+Options:`)
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	input, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := csv.NewReader(bytes.NewBuffer(input))
+	r.TrimLeadingSpace = !*flagNoTrim
+	r.Comma = []rune(*flagComma)[0]
+	rows, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var res interface{}
+	if !*flagNoHeader {
+		if len(rows) > 1 {
+			header := rows[0]
+			rows = rows[1:]
+			objs := make([]map[string]string, len(rows))
+			for y, row := range rows {
+				obj := map[string]string{}
+				for x, cell := range row {
+					obj[header[x]] = cell
+				}
+				objs[y] = obj
+			}
+			res = objs
+		} else {
+			res = []map[string]string{}
+		}
+	} else {
+		res = rows
+	}
+	output, err := json.Marshal(res)
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Stdout.Write(output)
+}
